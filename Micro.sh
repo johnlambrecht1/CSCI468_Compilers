89df3046223f0b8d0af6f6945f7802c9ac74e66e
bearfootcomputing@gmail.com <XnaJfjLh7bRLEAyVg7cn8mw(F9>
2020-02-14 19:11:20 -0700

    Fixed Micro.sh for use with Linux.Unix (wrong code for filename parameter for Linux, aliases don't work in script files, etc.)

--- Micro.sh
+++ Micro.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-# echo off
+#echo off
 clear
 
-# Run the program
-call antlr4 Step1.g4 -long-messages
-javac *.java
-java Driver %*
+# list aliases
+#alias -p
+
+# list exports (for CLASSPATH)
+#export -p
+
+# create CLASSPATH
+#export CLASSPATH=".:/home/jason/antlr4/antlr-4.8-complete.jar:$CLASSPATH"
+
+# Load grammar into Antlr4
+java -Xmx500M org.antlr.v4.Tool Step1.g4
+
+# compile java files
+javac *.java 
+
+# run java on Driver against input program file for grammar analysis
+java Driver $@
