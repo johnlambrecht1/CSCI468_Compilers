860307b6f023e646e2ea383485881ad093d22094
bearfootcomputing@gmail.com <XnaJfjLh7bRLEAyVg7cn8mw(F9>
2020-02-14 20:56:15 -0700

    Simplified Driver code, added comments to explain what coded does

--- Driver.java
+++ Driver.java
@@ -7,27 +7,45 @@ import java.io.FileWriter;
 
 public class Driver {
 
-    public static void main(String[] args) throws Exception     {
+    public static void main(String[] args) throws Exception {
 
-        String[] tokens = args[0].split("\\.");
-        // System.out.println( "tokens = " + tokens ); // debugging output
-        String file = tokens[0];
-        // System.out.println( "file = " + file ); // debugging output
+        // args[0] is the first argument and the filename with extension
+        // fullFileName is an array with the filename broken up into parts, name and extension
+        // file is just the name of the file, without extension
+
+        String[] fullFileName = args[0].split("\\.");
+        //System.out.println( "fullFileName = " + fullFileName ); // debugging output
+        //System.out.println( "args[0] = " + args[0] ); // debugging output
+        String file = fullFileName[0]; // File name is the first argument
+        //System.out.println( "file = " + file ); // debugging output
+
+        // chars is all of the characters in the file
         CharStream chars = CharStreams.fromFileName( "./" + args[0] );
-        // System.out.println( "chars = " + chars ); // debugging output
+        //System.out.println( "chars = " + chars ); // debugging output
+
+        // create the lexer from the chars
         Step1Lexer step1Lexer = new Step1Lexer( chars );
-        // System.out.println( "step1Lexer = " + step1Lexer ); // debugging output
+        //System.out.println( "step1Lexer = " + step1Lexer ); // debugging output
+
+        // vocabulary is the array of tokens, which can be broken down further
         Vocabulary vocabulary = step1Lexer.getVocabulary();
-        // System.out.println( "vocabulary = " + vocabulary ); // debugging output
+        //System.out.println( "vocabulary = " + vocabulary ); // debugging output
+
+        // create current for token use and move to next token
         Token current = step1Lexer.nextToken();
-	String output = "";
+
+        // output is used to simply screen listing and file storing of output
+        String output = "";
 
         // System.out.println( "Output to " + file ); // output notification
-        FileWriter fileWriter = new FileWriter( file + ".out"); // get file writer ready
 
+        // get file writer ready, use file.out as the destination
+        FileWriter fileWriter = new FileWriter( file + ".out");
+
+        // grab the initial tokenValue so that the while loop can work
         String tokenValue = current.getText() ;
 
-        while( tokenValue != "<EOF>" ){
+        while( tokenValue != "<EOF>" ) {
             // get token variables
             int tokenTypeNumber = current.getType() ; // get token number
             String tokenTypeName = vocabulary.getSymbolicName( tokenTypeNumber ) ; //convert token number to name
