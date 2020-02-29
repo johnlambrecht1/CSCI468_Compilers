bf0be9fdf384663843aa6c13c99c1f0b2ebed86d
John Lambrecht <jclambrecht97@msn.com>
2020-02-28 17:15:55 -0700

    changed file name

---
+++ Step1Listener.java
@@ -0,0 +1,19 @@
+// Generated from Step1.g4 by ANTLR 4.7.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link Step1Parser}.
+ */
+public interface Step1Listener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link Step1Parser#rg}.
+	 * @param ctx the parse tree
+	 */
+	void enterRg(Step1Parser.RgContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link Step1Parser#rg}.
+	 * @param ctx the parse tree
+	 */
+	void exitRg(Step1Parser.RgContext ctx);
+}
